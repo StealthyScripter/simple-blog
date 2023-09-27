532438dc76653afc2a801af7025e1cc1aa45347a
danielmendes24 <145878071+danielmendes24@users.noreply.github.com>
2023-09-27 07:39:00 -0300

    Did a responsive button for nav and sidebar

---
+++ script.js
@@ -0,0 +1,25 @@
+function clickMenu() {
+    if (Menu.style.display == 'flex') {
+        Menu.style.display = 'none'
+        document.getElementById('change').innerHTML = "menu"
+    } else {
+        Menu.style.display = 'flex'
+        document.getElementById('change').innerHTML = "close"
+        
+    }}
+
+function clickMenu2() {
+    if (Sidebar.style.display == 'block') {
+        Sidebar.style.display = 'none'
+    } else {
+        Sidebar.style.display = 'block'
+    }
+}
+
+function closeMenu() {
+    var Sidebar = document.getElementById('Sidebar');
+
+    if (Sidebar.style.display == 'block') {
+        Sidebar.style.display = 'none';
+    }
+}
